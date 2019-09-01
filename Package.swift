20e8ba9c77499b2e9cd7d530eedebdcfcc33864d
Eric Marchand <eric.marchand@4d.com>
2019-09-01 20:46:45 +0100

    [Mobile] Update FileKit to 6.0.0
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/SDK/": change = 241044]

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git" , from: "5.0.0"),
         .package(url: "https://github.com/Thomvis/BrightFutures.git" , from: "8.0.1"),
         .package(url: "https://github.com/weichsel/ZIPFoundation.git" , from: "0.9.9"),
-        .package(url: "https://github.com/nvzqz/FileKit.git" , .revision("48b5ddb287f131a5c628badc819b880453f94449")),
+        .package(url: "https://github.com/nvzqz/FileKit.git" , from: "6.0.0"),
         .package(url: "https://github.com/phimage/CallbackURLKit.git" , .revision("HEAD")),
         .package(url: "https://github.com/phimage/Prephirences.git" , .revision("HEAD")),
         .package(url: "https://github.com/ArtSabintsev/Guitar.git", from: "1.0.2")
