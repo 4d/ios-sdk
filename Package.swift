a482e4e458284afbfca7b7a64c3dfa8fd602ef08
quentin marciset <quentin.marciset@4d.com>
2019-09-30 08:14:35 +0100

    [Mobile] Updating packages versions #110828 - Moya from 14.0.0-beta.2 to 14.0.0-beta.3 - SwiftMessages from 7.0.0 to 7.0.1
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/SDK/": change = 242182]

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
 
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-rc.2"),
         .package(url: "https://github.com/DaveWoodCom/XCGLogger.git" , from: "7.0.0"),
-        .package(url: "https://github.com/Moya/Moya.git", from: "14.0.0-beta.2"),
+        .package(url: "https://github.com/Moya/Moya.git", from: "14.0.0-beta.3"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git" , from: "5.0.0"),
         .package(url: "https://github.com/Thomvis/BrightFutures.git" , from: "8.0.1"),
         .package(url: "https://github.com/weichsel/ZIPFoundation.git" , from: "0.9.9"),
