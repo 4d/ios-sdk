202386a3ebd7b739a3a04a32b356a7ea66a031be
Eric Marchand <eric.marchand@4d.com>
2019-10-02 12:09:03 +0100

    [Mobile] Update some third party framework: Prephirences, DeviceKit, KingFisher
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/SDK/": change = 242332]

--- Package.swift
+++ Package.swift
@@ -16,19 +16,19 @@ let package = Package(
         .package(url: "http://srv-git:3000/qmobile/QMobileUI.git" , .revision("HEAD")),
 
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-rc.2"),
-        .package(url: "https://github.com/DaveWoodCom/XCGLogger.git" , from: "7.0.0"),
+        .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.0"),
         .package(url: "https://github.com/Moya/Moya.git", from: "14.0.0-beta.3"),
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git" , from: "5.0.0"),
-        .package(url: "https://github.com/Thomvis/BrightFutures.git" , from: "8.0.1"),
-        .package(url: "https://github.com/weichsel/ZIPFoundation.git" , from: "0.9.9"),
-        .package(url: "https://github.com/nvzqz/FileKit.git" , from: "6.0.0"),
-        .package(url: "https://github.com/phimage/CallbackURLKit.git" , .revision("HEAD")),
-        .package(url: "https://github.com/phimage/Prephirences.git" , .revision("HEAD")),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
+        .package(url: "https://github.com/Thomvis/BrightFutures.git", from: "8.0.1"),
+        .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.9"),
+        .package(url: "https://github.com/nvzqz/FileKit.git", from: "6.0.0"),
+        .package(url: "https://github.com/phimage/CallbackURLKit.git", .revision("HEAD")),
+        .package(url: "https://github.com/phimage/Prephirences.git", from: "5.1.0")),
         .package(url: "https://github.com/ArtSabintsev/Guitar.git", from: "1.0.2")
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "5.8.1"),
+//        .package(url: "https://github.com/devicekit/DeviceKit.git", from: "2.3.0")), // not for macOS
 
 //        .package(url: "https://github.com/IBAnimatable/IBAnimatable.git", .revision("0776c5c099b308cd0cffe14f8cf89f0371153d03")), // not for macOS
-//        .package(url: "https://github.com/onevcat/Kingfisher.git" , .revision("68b7aa28a1d9f03ac00f2eeb0c522422dcd562bb")), // not for macOS
-//        .package(url: "https://github.com/devicekit/DeviceKit.git" , .revision("89452446badb4391899e989b8ae99c84488457f5")), // not for macOS
 //        .package(url: "https://github.com/SwiftKickMobile/SwiftMessages.git" , from: "7.0.0") // No Package.swift file
 //        .package(url: "https://github.com/phimage/ValueTransformerKit.git" , from: "1.2.1")
 //        .package(url: "https://github.com/xmartlabs/Eureka.git" , from: "5.0.0") // No Package.swift file
