e513ed0d99e5af88edc9bffe280f160db3d565f9
Eric Marchand <eric.marchand@4d.com>
2019-10-08 12:00:31 +0100

    [Mobile] Rename script sdksignsymbol to sdksign to sign also frameworks #110913
    Jobs: redmine:107769
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/SDK/": change = 242631]

--- sdksign.sh
+++ sdksign.sh
@@ -1,7 +1,9 @@
-
 nameCertificat=$1
 Entitlements=$2
-frameworks_folder=Carthage/Build/iOS
+frameworks_folder=$3
+if [ -z "$frameworks_folder" ]; then
+	frameworks_folder="Carthage/Build/iOS"
+fi
 signApp=buildChain/SignApp.sh
 
 # echo "strip arch "$arch
@@ -13,10 +15,15 @@ do
       extension="${framework##*.}"
 
       if [[ $extension = "framework" ]]; then
+	    # Sign symbol
         file_path=$framework".dSYM/Contents/Resources/DWARF/$name"
         if [ -f "$file_path" ]; then
            $signApp "$nameCertificat" "$file_path" "$Entitlements"
         fi
+        
+	    # Sign framework
+        file_path="$framework"
+        $signApp "$nameCertificat" "$file_path" "$Entitlements"
     fi
   fi
 done
