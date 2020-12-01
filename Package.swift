f3af55908d73e480eda074f69712f67648e65d24
Eric Marchand <eric.marchand@4d.com>
2020-12-01 19:34:01 +0100

    [Mobile] Add defaultLocalization in swiftPM files #122479
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/SDK/": change = 259032]

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SDK",
+    defaultLocalization: "en",
     platforms: [
         .macOS(.v10_15),
         .iOS(.v14)
