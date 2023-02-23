32b5fa27d35050488b97df873e4f4a5b12d706ac
eric.marchand <eric.marchand@4d.com>
2023-02-23 08:09:44 +0100

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,14 +15,14 @@ let package = Package(
         .package(url: "https://gitlab-4d.private.4d.fr/4d/qmobile/ios/QMobileDataStore.git" , .revision("HEAD")),
         .package(url: "https://gitlab-4d.private.4d.fr/4d/qmobile/ios/QMobileDataSync.git" , .revision("HEAD")),
         .package(url: "https://gitlab-4d.private.4d.fr/4d/qmobile/ios/QMobileUI.git" , .revision("HEAD")),
-        /*.package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0"),
+        /*.package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.6.4"),
         .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.0"),
-        .package(url: "https://github.com/Moya/Moya.git", from: "14.0.0"),
+        .package(url: "https://github.com/Moya/Moya.git", from: "15.0.3"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.9"),
-        .package(url: "https://github.com/nvzqz/FileKit.git", from: "6.0.0"),
+        .package(url: "https://github.com/nvzqz/FileKit.git", from: "6.1.0"),
         .package(url: "https://github.com/phimage/CallbackURLKit.git", .revision("HEAD")),
-        .package(url: "https://github.com/phimage/Prephirences.git", from: "5.1.0"),
+        .package(url: "https://github.com/phimage/Prephirences.git", from: "5.4.0"),
         .package(url: "https://github.com/onevcat/Kingfisher.git", from: "5.13.4")
         .package(url: "https://github.com/phimage/ValueTransformerKit.git" , from: "1.2.3")
 //        .package(url: "https://github.com/SwiftKickMobile/SwiftMessages.git", from: "7.0.0"), // No Package.swift file
